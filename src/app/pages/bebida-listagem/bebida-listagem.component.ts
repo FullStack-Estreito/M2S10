75c582d8e05e912cf790b76266ef97f2c4eb8bb4
Kelvis Borges <kelvisbcc@gmail.com>
2023-07-06 21:47:12 -0300

    ✨ feat(bebida): adiciona service e preenche lista

--- src/app/pages/bebida-listagem/bebida-listagem.component.ts
+++ src/app/pages/bebida-listagem/bebida-listagem.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
-import { BEBIDAS_MOCK } from "src/app/shared/mocks/bebida-mock";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { BebidaItem } from "src/app/shared/models/bebida-item.model";
+import { AppService } from "src/app/shared/services/app.service";
 
 @Component({
   selector: "nfd-bebida-listagem",
@@ -7,5 +9,7 @@ import { BEBIDAS_MOCK } from "src/app/shared/mocks/bebida-mock";
   styleUrls: ["./bebida-listagem.component.scss"],
 })
 export class BebidaListagemComponent {
-  bebidaLista = BEBIDAS_MOCK;
+  bebidaLista$: Observable<BebidaItem[]> = this.bebidaService.getBebidas();
+
+  constructor(private bebidaService: AppService) {}
 }
